9c8efab9b698e7be0ec15b1776392efbb5f63005
mileRANT <liem.htran@outlook.com>
2023-05-12 15:46:21 -0400

    Initial commit. set up basics with npm, bodyparser, express, ejs

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.send("Hello World");
+});
+
+app.listen(3000, function(){
+    console.log("Listening on port 3000");
+});
